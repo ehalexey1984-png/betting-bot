10a4b5f596dc6fc863a07f0ef7d023aafe66b0d0
ehalexey1984-png <ehalexey1984@gmail.com>
2026-06-14 12:03:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY")
+
+HEADERS = {"X-Auth-Token": FOOTBALL_API_KEY}
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот запущен. Используй /today")
+
+async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = "https://api.football-data.org/v4/matches"
+
+    r = requests.get(url, headers=HEADERS)
+    data = r.json()
+
+    matches = data.get("matches", [])
+
+    if not matches:
+        await update.message.reply_text("Нет матчей сегодня")
+        return
+
+    text = "Матчи сегодня:\n\n"
+
+    for m in matches[:15]:
+        home = m["homeTeam"]["name"]
+        away = m["awayTeam"]["name"]
+        comp = m["competition"]["name"]
+
+        text += f"{comp}\n{home} vs {away}\n\n"
+
+    await update.message.reply_text(text)
+
+def main():
+    app = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("today", today))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
